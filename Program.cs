b5e3a88f83de6417df16f67880ef0e32bacadde5
Khias Nurlatif Ari Subekti <57320303+qazolatif@users.noreply.github.com>
2020-03-19 19:08:42 +0700

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+namespace TugasClassTaxi
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+            
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+            
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+            
+            Console.ReadKey();
+        }
+    }
+}
